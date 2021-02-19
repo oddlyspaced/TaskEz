3c7973192015a4c994bcf9a29b7e93fcc6569ce6
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-20 01:21:12 +0530

    OnboardingActivity: Init

---
+++ app/src/main/java/com/oddlyspaced/taskez/activity/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.taskez.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.taskez.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
