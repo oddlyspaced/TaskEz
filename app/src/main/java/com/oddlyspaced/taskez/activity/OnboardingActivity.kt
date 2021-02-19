e41eaaf73e748a7b4ecd250678bfabc06033763f
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-20 01:44:54 +0530

    OnboardingActivity: Display OnboardingFragment1

--- app/src/main/java/com/oddlyspaced/taskez/activity/OnboardingActivity.kt
+++ app/src/main/java/com/oddlyspaced/taskez/activity/OnboardingActivity.kt
@@ -3,10 +3,22 @@ package com.oddlyspaced.taskez.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.taskez.R
+import com.oddlyspaced.taskez.databinding.ActivityOnboardingBinding
+import com.oddlyspaced.taskez.fragment.OnboardingFragment1
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_onboarding)
+
+        supportFragmentManager.beginTransaction().apply {
+            add(binding.frameOnboardingFragmentHolder.id, OnboardingFragment1.newInstance(), OnboardingFragment1::class.simpleName)
+            addToBackStack(OnboardingFragment1::class.simpleName)
+        }.commit()
+
     }
 }
