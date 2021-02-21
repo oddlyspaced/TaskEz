045713219616af51071dd6d1049da79fc3aa62c7
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-21 13:53:47 +0530

    AccountActivity: Launch AccountActivity

--- app/src/main/java/com/oddlyspaced/taskez/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/taskez/MainActivity.kt
@@ -3,13 +3,13 @@ package com.oddlyspaced.taskez
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.oddlyspaced.taskez.activity.OnboardingActivity
+import com.oddlyspaced.taskez.activity.AccountActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        startActivity(Intent(applicationContext, OnboardingActivity::class.java))
+        startActivity(Intent(applicationContext, AccountActivity::class.java))
     }
 }
