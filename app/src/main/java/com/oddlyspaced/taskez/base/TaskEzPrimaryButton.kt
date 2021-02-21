7e62d443a8958ce222f1c5ba150fb7164fec4c28
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-21 20:42:16 +0530

    TaskEzPrimaryButton: Read attributes and make mods to binding accordingly

--- app/src/main/java/com/oddlyspaced/taskez/base/TaskEzPrimaryButton.kt
+++ app/src/main/java/com/oddlyspaced/taskez/base/TaskEzPrimaryButton.kt
@@ -5,9 +5,13 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.LayoutInflater
 import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import com.oddlyspaced.taskez.R
+import com.oddlyspaced.taskez.databinding.ButtonPrimaryBinding
 
 class TaskEzPrimaryButton: CardView {
+
+    private lateinit var binding: ButtonPrimaryBinding
 
     constructor(ctx: Context) : super(ctx) {
         init(ctx)
@@ -15,17 +19,36 @@ class TaskEzPrimaryButton: CardView {
 
     constructor(ctx: Context, attr: AttributeSet) : super(ctx, attr) {
         init(ctx)
+        setAttr(attr)
     }
 
     constructor(ctx: Context, attr: AttributeSet, defStyleAttr: Int) : super(ctx, attr, defStyleAttr) {
         init(ctx)
+        setAttr(attr)
     }
 
     private fun init(context: Context) {
         LayoutInflater.from(context).inflate(R.layout.button_primary, this, true)
-        this.setCardBackgroundColor(context.getColor(R.color.primary))
-        this.elevation = floatToDp(8F)
-        this.radius = floatToDp(24F)
+        binding = ButtonPrimaryBinding.bind(this)
+        // TODO : Check why double setting is necessary
+        binding.cvPrimaryButton.setCardBackgroundColor(context.getColor(R.color.primary))
+        binding.cvPrimaryButton.elevation = floatToDp(8F)
+        binding.cvPrimaryButton.radius = floatToDp(24F)
+    }
+
+    private fun setAttr(attributes: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attributes, R.styleable.TaskEzPrimaryButton, 0, 0)
+        try {
+            binding.imgPrimaryButtonDrawable.isVisible = typedArray.getBoolean(R.styleable.TaskEzPrimaryButton_disableIcon, false)
+            binding.txPrimaryButtonText.text = typedArray.getString(R.styleable.TaskEzPrimaryButton_text)
+            binding.imgPrimaryButtonDrawable.setImageResource(typedArray.getResourceId(R.styleable.TaskEzPrimaryButton_iconSrc, R.drawable.ic_mail))
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        finally {
+            typedArray.recycle()
+        }
     }
 
     private fun floatToDp(dp: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
