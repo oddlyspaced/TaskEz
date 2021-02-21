22170846ff228f3e719e6507d82819b1c45cbb09
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-21 20:51:23 +0530

    AskEmailFragment: Load sign up layout

--- app/src/main/java/com/oddlyspaced/taskez/fragment/AskEmailFragment.kt
+++ app/src/main/java/com/oddlyspaced/taskez/fragment/AskEmailFragment.kt
@@ -17,7 +17,7 @@ class AskEmailFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_ask_email, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
 }
