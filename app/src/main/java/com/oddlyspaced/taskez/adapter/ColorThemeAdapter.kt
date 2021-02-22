41d23af39b0901e6689f9002f9289c243420355e
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-23 00:41:37 +0530

    ColorThemeAdapter: Add click listener and keep track of dynamic active item

--- app/src/main/java/com/oddlyspaced/taskez/adapter/ColorThemeAdapter.kt
+++ app/src/main/java/com/oddlyspaced/taskez/adapter/ColorThemeAdapter.kt
@@ -9,7 +9,7 @@ import com.oddlyspaced.taskez.databinding.ItemColorThemeBinding
 import com.oddlyspaced.taskez.modal.ColorThemeItem
 import com.oddlyspaced.taskez.utils.DimensionUtils
 
-class ColorThemeAdapter(private val list: ArrayList<ColorThemeItem>): RecyclerView.Adapter<ColorThemeAdapter.ViewHolder>() {
+class ColorThemeAdapter(private val list: ArrayList<ColorThemeItem>, var selectedIndex: Int = 0): RecyclerView.Adapter<ColorThemeAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val binding: ItemColorThemeBinding = ItemColorThemeBinding.bind(itemView)
@@ -23,13 +23,16 @@ class ColorThemeAdapter(private val list: ArrayList<ColorThemeItem>): RecyclerVi
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = list[position]
-        if (item.isSelected) {
-            holder.card.strokeWidth = DimensionUtils.floatToDp(holder.itemView.context, 2F).toInt()
-        }
+        holder.card.strokeWidth = DimensionUtils.floatToDp(holder.itemView.context, if (position == selectedIndex) 2F else 0F).toInt()
         holder.gradient.setBackgroundResource(item.resource)
+        holder.card.setOnClickListener {
+            val bkp = selectedIndex
+            selectedIndex = position
+            notifyItemChanged(bkp)
+            notifyItemChanged(selectedIndex)
+        }
     }
 
     override fun getItemCount(): Int = list.size
-
 
 }
