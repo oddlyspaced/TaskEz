bece02fd0709e1dc3c6ccff499809c5c6b984a22
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-23 00:29:32 +0530

    ColorThemeAdapter: Setup base view hold and bind data to items

--- app/src/main/java/com/oddlyspaced/taskez/adapter/ColorThemeAdapter.kt
+++ app/src/main/java/com/oddlyspaced/taskez/adapter/ColorThemeAdapter.kt
@@ -5,11 +5,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.oddlyspaced.taskez.R
+import com.oddlyspaced.taskez.databinding.ItemColorThemeBinding
 import com.oddlyspaced.taskez.modal.ColorThemeItem
+import com.oddlyspaced.taskez.utils.DimensionUtils
 
 class ColorThemeAdapter(private val list: ArrayList<ColorThemeItem>): RecyclerView.Adapter<ColorThemeAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val binding: ItemColorThemeBinding = ItemColorThemeBinding.bind(itemView)
+        val card = binding.cvColorTheme
+        val gradient = binding.consColorThemeGradient
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -17,7 +22,11 @@ class ColorThemeAdapter(private val list: ArrayList<ColorThemeItem>): RecyclerVi
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        val item = list[position]
+        if (item.isSelected) {
+            holder.card.strokeWidth = DimensionUtils.floatToDp(holder.itemView.context, 2F).toInt()
+        }
+        holder.gradient.setBackgroundResource(item.resource)
     }
 
     override fun getItemCount(): Int = list.size
