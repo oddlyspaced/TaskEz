9bb8f6a3ad1d38cbd95aaf9d25881c1a4928496e
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-23 00:11:33 +0530

    MainActivity: Launch WorkspaceActivity instead

--- app/src/main/java/com/oddlyspaced/taskez/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/taskez/MainActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.oddlyspaced.taskez.activity.AccountActivity
+import com.oddlyspaced.taskez.activity.WorkspaceActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        startActivity(Intent(applicationContext, AccountActivity::class.java))
+        startActivity(Intent(applicationContext, WorkspaceActivity::class.java))
     }
 }
