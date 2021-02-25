04f4527654649939ab22b60df5ca3da3d25bfb4c
oddlyspaced <oddlyspaced1@gmail.com>
2021-02-26 04:31:20 +0530

    TaskEzProgressBar: Setup progress value in class for update

--- app/src/main/java/com/oddlyspaced/taskez/base/TaskEzProgressBar.kt
+++ app/src/main/java/com/oddlyspaced/taskez/base/TaskEzProgressBar.kt
@@ -15,43 +15,43 @@ class TaskEzProgressBar: ConstraintLayout {
 
     private lateinit var binding: ProgressBarBinding
 
+    var progress: Float = 0.2F
+    set(value) {
+        field = value
+        if (this::binding.isInitialized) {
+            binding.guidelineProgressBarPercentage.setGuidelinePercent(value)
+        }
+    }
+
     constructor(ctx: Context) : super(ctx) {
         init(ctx)
     }
 
     constructor(ctx: Context, attr: AttributeSet) : super(ctx, attr) {
         init(ctx)
-//        setAttr(attr)
+        setAttr(attr)
     }
 
     constructor(ctx: Context, attr: AttributeSet, defStyleAttr: Int) : super(ctx, attr, defStyleAttr) {
         init(ctx)
-//        setAttr(attr)
+        setAttr(attr)
     }
 
     private fun init(context: Context) {
         binding = ProgressBarBinding.inflate(LayoutInflater.from(context), this, true)
-        binding.guidelineProgressBarPercentage.setGuidelinePercent(0.2F)
     }
 
-//    private fun setAttr(attributes: AttributeSet) {
-//        val typedArray = context.obtainStyledAttributes(attributes, R.styleable.TaskEzPrimaryButton, 0, 0)
-//        try {
-//            binding.imgPrimaryButtonDrawable.isVisible = typedArray.getBoolean(R.styleable.TaskEzPrimaryButton_iconVisible, false)
-//            binding.txPrimaryButtonText.text = typedArray.getString(R.styleable.TaskEzPrimaryButton_text)
-//            binding.imgPrimaryButtonDrawable.setImageResource(typedArray.getResourceId(R.styleable.TaskEzPrimaryButton_iconSrc, R.drawable.ic_mail))
-//            if (typedArray.getBoolean(R.styleable.TaskEzPrimaryButton_deactivated, false)) {
-//                this.setCardBackgroundColor(context.getColor(R.color.background_2))
-//                binding.txPrimaryButtonText.setTextColor(context.getColor(R.color.state_deactivated))
-//                this.elevation = DimensionUtils.floatToDp(context, 0F)
-//            }
-//        }
-//        catch (e: Exception) {
-//            e.printStackTrace()
-//        }
-//        finally {
-//            typedArray.recycle()
-//        }
-//    }
+    private fun setAttr(attributes: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attributes, R.styleable.TaskEzProgressBar, 0, 0)
+        try {
+            binding.guidelineProgressBarPercentage.setGuidelinePercent(typedArray.getFloat(R.styleable.TaskEzProgressBar_progress, 0F))
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        finally {
+            typedArray.recycle()
+        }
+    }
 
 }
