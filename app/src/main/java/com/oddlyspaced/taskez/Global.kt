ead395facd9bed88e55100e054a63d14c2b3950e
oddlyspaced <oddlyspaced1@gmail.com>
2021-03-01 03:00:44 +0530

    Global: Create global constant class with gradient list

---
+++ app/src/main/java/com/oddlyspaced/taskez/Global.kt
@@ -0,0 +1,15 @@
+package com.oddlyspaced.taskez
+
+class Global {
+    companion object {
+        val gradientList = arrayOf(
+            R.drawable.gradient_1,
+            R.drawable.gradient_2,
+            R.drawable.gradient_3,
+            R.drawable.gradient_4,
+            R.drawable.gradient_7,
+            R.drawable.gradient_8,
+            R.drawable.gradient_9
+        )
+    }
+}
