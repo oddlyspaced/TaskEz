23d62606e3fe4cf9f1430e01527d86b4bade02da
oddlyspaced <oddlyspaced1@gmail.com>
2021-03-01 03:01:46 +0530

    DashboardActivity: Display basic card list

--- app/src/main/java/com/oddlyspaced/taskez/activity/DashboardActivity.kt
+++ app/src/main/java/com/oddlyspaced/taskez/activity/DashboardActivity.kt
@@ -3,11 +3,22 @@ package com.oddlyspaced.taskez.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.taskez.R
+import com.oddlyspaced.taskez.adapter.TaskStackAdapter
 import com.oddlyspaced.taskez.base.TaskEzActivity
+import com.oddlyspaced.taskez.databinding.ActivityDashboardBinding
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 
 class DashboardActivity : TaskEzActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cardStackDashboard.layoutManager = CardStackLayoutManager(applicationContext)
+        binding.cardStackDashboard.adapter = TaskStackAdapter()
+
     }
 }
