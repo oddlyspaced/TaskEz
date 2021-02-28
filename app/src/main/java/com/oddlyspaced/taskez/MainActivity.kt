68e9fce4debe78a3ecb8bcfde348eda27ba2cad9
oddlyspaced <oddlyspaced1@gmail.com>
2021-03-01 02:46:49 +0530

    MainActivity: Launch WorkspaceActivity

--- app/src/main/java/com/oddlyspaced/taskez/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/taskez/MainActivity.kt
@@ -2,15 +2,14 @@ package com.oddlyspaced.taskez
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.oddlyspaced.taskez.activity.AccountActivity
-import com.oddlyspaced.taskez.activity.WorkspaceActivity
+import com.oddlyspaced.taskez.activity.DashboardActivity
+import com.oddlyspaced.taskez.base.TaskEzActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : TaskEzActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        startActivity(Intent(applicationContext, WorkspaceActivity::class.java))
+        startActivity(Intent(applicationContext, DashboardActivity::class.java))
     }
 }
