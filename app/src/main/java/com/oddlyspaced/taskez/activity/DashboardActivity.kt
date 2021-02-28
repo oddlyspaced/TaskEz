4b662bf98c904fd5c627b8b78028dfcb4a859933
oddlyspaced <oddlyspaced1@gmail.com>
2021-03-01 02:46:32 +0530

    DashboardActivity: Layout base

--- app/src/main/java/com/oddlyspaced/taskez/activity/DashboardActivity.kt
+++ app/src/main/java/com/oddlyspaced/taskez/activity/DashboardActivity.kt
@@ -3,8 +3,9 @@ package com.oddlyspaced.taskez.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.taskez.R
+import com.oddlyspaced.taskez.base.TaskEzActivity
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : TaskEzActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
